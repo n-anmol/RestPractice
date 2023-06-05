1efc886d475a3d53328f3a4ca730ff875f3daf9d
n-anmol <n.anmol@capgemini.com>
2023-06-05 10:52:02 +0530

    Swagger working

--- src/main/java/com/nanmol/sbr/restPractice/controller/RestControllerClass.java
+++ src/main/java/com/nanmol/sbr/restPractice/controller/RestControllerClass.java
@@ -2,6 +2,7 @@ package com.nanmol.sbr.restPractice.controller;
 
 import com.nanmol.sbr.restPractice.dao.DaoClass;
 import com.nanmol.sbr.restPractice.entities.Customer;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/customer")
+@Tag(name = "Customer API", description = "Customer REST API")
 public class RestControllerClass {
     @Autowired
     private DaoClass dao;
